59cdd3e211305ba55a7c4e7caf3a4872d016f68f
spadix <spadix@users.sourceforge.net>
2010-08-26 18:28:48 -0400

    iPhone: fix missing header dependency

--- iphone/include/ZBarSDK/ZBarSymbol.h
+++ iphone/include/ZBarSDK/ZBarSymbol.h
@@ -22,6 +22,7 @@
 //------------------------------------------------------------------------
 
 #import <Foundation/Foundation.h>
+#import <CoreGraphics/CoreGraphics.h>
 #import "zbar.h"
 
 #ifdef __cplusplus
